695e1e9972de7985aeed146b665acfd62df790c1
melisa gocay <mgocay@knights.ucf.edu>
2019-07-22 22:37:16 -0400

    15th

--- SpaceShooter/Assets/Scripts/DestroyByContact.cs
+++ SpaceShooter/Assets/Scripts/DestroyByContact.cs
@@ -4,8 +4,24 @@ using UnityEngine;
 
 public class DestroyByContact : MonoBehaviour
 {
+    private GameController gameController;
+    public int scoreValue;
     public GameObject explosion;
     public GameObject playerExplosion;
+
+    void Start ()
+    {
+        GameObject gameControllerObject = GameObject.FindWithTag ("GameController");
+        if (gameControllerObject != null)
+        {
+            gameController = gameControllerObject.GetComponent <GameController>();
+        }
+        if (gameController == null)
+        {
+            Debug.Log ("Cannot find 'GameController' script");
+        }
+    }
+
     void OnTriggerEnter(Collider other)
     {
         if(other.tag == "Boundary")
@@ -16,7 +32,9 @@ public class DestroyByContact : MonoBehaviour
         if (other.tag == "Player")
         {
             Instantiate (playerExplosion, other.transform.position, other.transform.rotation);
+            gameController.GameOver ();
         }
+        gameController.AddScore (scoreValue);
         Destroy(other.gameObject);
         Destroy(gameObject);
     }
--- SpaceShooter/Assets/Scripts/GameController.cs
+++ SpaceShooter/Assets/Scripts/GameController.cs
@@ -1,35 +1,83 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class GameController : MonoBehaviour
 {
-    public GameObject hazard;
-    public Vector3 spawnValues;
-    public int hazardCount; 
-    public float spawnWait;
-    public float startWait;
-    public float waveWait;
-    void Start ()
-    {
-        StartCoroutine (SpawnWaves ());
-    }
+public GameObject hazard;
+public Vector3 spawnValues;
+public int hazardCount;
+public float spawnWait;
+public float startWait;
+public float waveWait;
 
-    IEnumerator SpawnWaves()
+public Text ScoreText;
+public Text restartText;
+public Text gameOverText;
+private bool gameOver;
+private bool restart;
+private int score;
+
+void Start()
+{
+    gameOver = false;
+    restart = false;
+    restartText.text = "";
+    gameOverText.text = "";
+score = 0;
+UpdateScore();
+StartCoroutine(SpawnWaves());
+}
+void Update()
+{
+    if (restart)
     {
-        yield return new WaitForSeconds (startWait);
-        while (true)
+        if (Input.GetKeyDown (KeyCode.R))
         {
-    
-            for (int i = 0; 1 < hazardCount; i++)
-            { 
-
-                Vector3 spawnPosition = new Vector3 (Random.Range (-spawnValues.x, spawnValues.x), spawnValues.y, spawnValues.z);
-                Quaternion spawnRotation = Quaternion.identity;
-                Instantiate (hazard, spawnPosition, spawnRotation);
-                yield return new WaitForSeconds (spawnWait);
-            }
-            yield return new WaitForSeconds (waveWait);
+            SceneManager.LoadScene("Main");
         }
     }
+    if (Input.GetKey("escape"))
+     Application.Quit();
+
+}
+
+IEnumerator SpawnWaves()
+{
+yield return new WaitForSeconds(startWait);
+while (true)
+{
+for (int i = 0; i < hazardCount; i++)
+{
+Vector3 spawnPosition = new Vector3(Random.Range(-spawnValues.x, spawnValues.x), spawnValues.y, spawnValues.z);
+Quaternion spawnRotation = Quaternion.identity;
+Instantiate(hazard, spawnPosition, spawnRotation);
+yield return new WaitForSeconds(spawnWait);
+}
+yield return new WaitForSeconds(waveWait);
+if (gameOver)
+{
+    restartText.text = "Press 'R' for Restart";
+    restart = true;
+    break;
+}
+}
+}
+
+public void AddScore(int newScoreValue)
+{
+score += newScoreValue;
+UpdateScore();
+}
+public void GameOver ()
+{
+    gameOverText.text = "Game Over!";
+    gameOver = true;
+}
+
+void UpdateScore()
+{
+ScoreText.text = "Score: " + score;
+}
 }
