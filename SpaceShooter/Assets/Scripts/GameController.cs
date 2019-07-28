127e1059f4a936f7aa54217914b70b679ac47ae3
melisa gocay <mgocay@knights.ucf.edu>
2019-07-28 16:12:20 -0400

    1st tier done

--- SpaceShooter/Assets/Scripts/GameController.cs
+++ SpaceShooter/Assets/Scripts/GameController.cs
@@ -5,85 +5,86 @@ using UnityEngine.SceneManagement;
 
 public class GameController : MonoBehaviour
 {
-public GameObject hazard;
-public Vector3 spawnValues;
-public int hazardCount;
-public float spawnWait;
-public float startWait;
-public float waveWait;
+    public GameObject[] hazards;
+    public Vector3 spawnValues;
+    public int hazardCount;
+    public float spawnWait;
+    public float startWait;
+    public float waveWait;
 
-public Text ScoreText;
-public Text restartText;
-public Text gameOverText;
-private bool gameOver;
-private bool restart;
-private int score;
+    public Text ScoreText;
+    public Text restartText;
+    public Text gameOverText;
+    private bool gameOver;
+    private bool restart;
+    private int score;
 
-void Start()
-{
-    gameOver = false;
-    restart = false;
-    restartText.text = "";
-    gameOverText.text = "";
-score = 0;
-UpdateScore();
-StartCoroutine(SpawnWaves());
-}
-void Update()
-{
-    if (restart)
+    void Start()
     {
-        if (Input.GetKeyDown (KeyCode.L))
+        gameOver = false;
+        restart = false;
+        restartText.text = "";
+        gameOverText.text = "";
+        score = 0;
+        UpdateScore();
+        StartCoroutine(SpawnWaves());   
+    }
+    void Update()
+    {
+        if (restart)
         {
-            SceneManager.LoadScene("Main");
+            if (Input.GetKeyDown (KeyCode.L))
+            {
+                SceneManager.LoadScene("Main");
+            }
+        }
+        if (Input.GetKey("escape"))
+            Application.Quit();
+
+    }
+
+    IEnumerator SpawnWaves()
+    {
+        yield return new WaitForSeconds(startWait);
+        while (true)
+        {
+            for (int i = 0; i < hazardCount; i++)
+                {   
+                    GameObject hazard = hazards [Random.Range (0, hazards.Length)];
+                    Vector3 spawnPosition = new Vector3(Random.Range(-spawnValues.x, spawnValues.x), spawnValues.y, spawnValues.z);
+                    Quaternion spawnRotation = Quaternion.identity;
+                    Instantiate(hazard, spawnPosition, spawnRotation);
+                    yield return new WaitForSeconds(spawnWait);
+                }
+            yield return new WaitForSeconds(waveWait);
+            if (gameOver)
+                {
+                    restartText.text = "Press 'L' for Restart";
+                    restart = true;
+                    break;
+                }
         }
     }
-    if (Input.GetKey("escape"))
-     Application.Quit();
 
-}
-
-IEnumerator SpawnWaves()
-{
-yield return new WaitForSeconds(startWait);
-while (true)
-{
-for (int i = 0; i < hazardCount; i++)
-{
-Vector3 spawnPosition = new Vector3(Random.Range(-spawnValues.x, spawnValues.x), spawnValues.y, spawnValues.z);
-Quaternion spawnRotation = Quaternion.identity;
-Instantiate(hazard, spawnPosition, spawnRotation);
-yield return new WaitForSeconds(spawnWait);
-}
-yield return new WaitForSeconds(waveWait);
-if (gameOver)
-{
-    restartText.text = "Press 'L' for Restart";
-    restart = true;
-    break;
-}
-}
-}
-
-public void AddScore(int newScoreValue)
-{
-score += newScoreValue;
-UpdateScore();
-}
-public void GameOver ()
-{
-    gameOverText.text = "Game Over! Game Created by Melisa";
-    gameOver = true;
-}
-
-void UpdateScore()
+    public void AddScore(int newScoreValue)
     {
-        ScoreText.text = "Points: " + score;
-        if (score >= 100)
-        {
-            gameOverText.text = "You win! Game Created By Melisa";
-            gameOver = true;
-            restart = true;
-        }
+        score += newScoreValue;
+        UpdateScore();
     }
+    public void GameOver ()
+    {
+        gameOverText.text = "Game Over! Game Created by Melisa";
+        gameOver = true;
+    }
+
+    void UpdateScore()
+        {
+            ScoreText.text = "Points: " + score;
+            if (score >= 100)
+            {
+                gameOverText.text = "You win! Game Created By Melisa";
+                gameOver = true;
+                restart = true;
+            }
+        }
 }
